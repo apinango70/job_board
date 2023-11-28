1b03eed3c8b1e784f903de8d72efbcc1328dd5f3
Alejandro Pinango <alejandropinango@gmail.com>
2023-11-28 19:21:03 -0300

    feat: modificar registration, application controller y modelo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Enum de roles
+  enum role: [:user, :recruiter, :admin]
+
 end
