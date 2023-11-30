e832c1d5e197917615a174d4b617677c9927f1fc
Alejandro Pinango <alejandropinango@gmail.com>
2023-11-30 19:06:11 -0300

    feat: crear modelo job_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_224656) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_30_220533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_224656) do
     t.string "website"
     t.string "location"
     t.text "about"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "job_types", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
